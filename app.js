ddc8b25038e7c7ada984beef8ae4eb7bb31ed450
Dawson Toth <dtoth@appcelerator.com>
2014-12-16 13:49:13 -0500

    https://jira.appcelerator.org/browse/API-230

--- app.js
+++ app.js
@@ -1,38 +1,10 @@
+/**
+ * NOTE: This file is simply for testing this connector and will not
+ * be used or packaged with the actual connector when published.
+ */
 var APIBuilder = require('appcelerator').apibuilder,
 	server = new APIBuilder();
 
-// lifecycle examples
-server.on('starting', function() {
-	server.logger.info('server is starting!');
-});
-
-server.on('started', function() {
-	server.logger.info('server started!');
-});
-
-// fetch our configured apikey
-var apikey = server.get('apikey');
-server.logger.info('APIKey is:', apikey);
-
-function APIKeyAuthorization(req, resp, next) {
-	if (!apikey) {
-		return next();
-	}
-	if (req.headers['apikey']) {
-		var key = req.headers['apikey'];
-		if (key === apikey) {
-			return next();
-		}
-	}
-	resp.status(401);
-	return resp.json({
-		id: "com.appcelerator.api.unauthorized",
-		message: "Unauthorized",
-		url: ""
-	});
-}
-
-// create a post api from a post model
 var Post = APIBuilder.Model.extend('post', {
 	fields: {
 		title: { type: String },
@@ -42,16 +14,11 @@ var Post = APIBuilder.Model.extend('post', {
 });
 server.addModel(Post);
 
-// add an authorization policy for all requests at the server log
-server.authorization = APIKeyAuthorization;
-
-// start the server
 server.start(function(err) {
 	if (err) { server.logger.fatal(err); }
 	server.logger.info('server started on port', server.port);
 
-
-	// create some posts programmatically
+	// Create some posts programmatically.
 	var posts = [
 		{ title: 'A journey through time', content: 'take a journey' },
 		{ title: 'The Bible', content: 'In the beginning, God created the heavens and the earth.' }
