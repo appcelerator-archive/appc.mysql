71b6aba4896d102c05f3c1d2526a3a7c079a825a
Milan Zarkov <mzarkov@item-ax30134.wks.axway.int>
2017-03-13 11:01:42 +0200

    Test for findAll, ditinct, fetchMetadata methods

---
+++ test/unit/methods/distinct.js
@@ -0,0 +1,147 @@
+const test = require('tap').test
+const server = require('../../server')
+const distinctMethod = require('./../../../lib/methods/distinct').distinct
+const sinon = require('sinon')
+var ARROW
+var CONNECTOR
+
+test('### Start Arrow ###', function (t) {
+  server()
+    .then((inst) => {
+      ARROW = inst
+      CONNECTOR = ARROW.getConnector('appc.mysql')
+      t.ok(ARROW, 'Arrow has been started')
+      t.end()
+    })
+    .catch((err) => {
+      t.threw(err)
+    })
+})
+
+test('### Distinct method response ###', sinon.test(function (t) {
+  const Model = ARROW.getModel('Posts')
+  const data = 'Test distinct method'
+  function cb(errorMessage, data) { }
+  const cbSpy = this.spy(cb)
+
+  const tableNameStub = this.stub(
+    CONNECTOR,
+    'getTableName',
+    (Model) => {
+      return 'post'
+    }
+  )
+
+  const translateWhereToQueryStub = this.stub(
+    CONNECTOR,
+    'translateWhereToQuery',
+    (where, values) => {
+      return true
+    }
+  )
+
+  const queryStub = this.stub(
+    CONNECTOR,
+    '_query',
+    (query, values, callback, executor) => {
+      executor([1, 2, 3])
+    }
+  )
+
+  distinctMethod.bind(CONNECTOR, Model, 'post', {}, cbSpy)()
+
+  t.ok(queryStub.calledOnce)
+  t.ok(cbSpy.calledOnce)
+
+  t.end()
+}))
+
+test('### Distinct method not unique records ###', sinon.test(function (t) {
+  const Model = ARROW.getModel('Posts')
+  const data = 'Test distinct method'
+  function cb(errorMessage, data) { }
+  const cbSpy = this.spy(cb)
+
+  const tableNameStub = this.stub(
+    CONNECTOR,
+    'getTableName',
+    (Model) => {
+      return 'post'
+    }
+  )
+
+  const translateWhereToQueryStub = this.stub(
+    CONNECTOR,
+    'translateWhereToQuery',
+    (where, values) => {
+      return true
+    }
+  )
+
+  const queryStub = this.stub(
+    CONNECTOR,
+    '_query',
+    (query, values, callback, executor) => {
+      executor(undefined)
+    }
+  )
+
+  distinctMethod.bind(CONNECTOR, Model, 'post', {}, cbSpy)()
+
+  t.ok(queryStub.calledOnce)
+  t.ok(cbSpy.calledOnce)
+
+  t.end()
+}))
+
+test('### Distinct method with where clause ###', sinon.test(function (t) {
+  const Model = ARROW.getModel('Posts')
+  const data = 'Test distinct method'
+  function cb(errorMessage, data) { }
+  const cbSpy = this.spy(cb)
+
+  const tableNameStub = this.stub(
+    CONNECTOR,
+    'getTableName',
+    (Model) => {
+      return 'post'
+    }
+  )
+
+  const translateWhereToQueryStub = this.stub(
+    CONNECTOR,
+    'translateWhereToQuery',
+    (where, values) => {
+      return { "$eq": "Test" }
+    }
+  )
+
+  const queryStub = this.stub(
+    CONNECTOR,
+    '_query',
+    (query, values, callback, executor) => {
+      executor(undefined)
+    }
+  )
+
+  const options = {
+    where: {
+      Name: { "$eq": "Test" }
+    }
+  }
+
+  distinctMethod.bind(CONNECTOR, Model, 'post', options, cbSpy)()
+
+  t.ok(queryStub.calledOnce)
+  t.ok(cbSpy.calledOnce)
+
+  t.end()
+}))
+
+test('### Stop Arrow ###', function (t) {
+
+  ARROW.stop(function () {
+    t.pass('Arrow has been stopped!')
+    t.end()
+  })
+})
---
+++ test/unit/methods/findAll.js
@@ -0,0 +1,156 @@
+const test = require('tap').test
+const server = require('../../server')
+const findAllMethod = require('./../../../lib/methods/findAll').findAll
+const sinon = require('sinon')
+const arrow = require('arrow')
+var ARROW
+var CONNECTOR
+
+test('### Start Arrow ###', function (t) {
+  server()
+    .then((inst) => {
+      ARROW = inst
+      CONNECTOR = ARROW.getConnector('appc.mysql')
+      t.ok(ARROW, 'Arrow has been started')
+      t.end()
+    })
+    .catch((err) => {
+      t.threw(err)
+    })
+})
+
+test('### Test findAll Method ###', sinon.test(function (t) {
+  const Model = ARROW.getModel('Posts')
+  const data = 'Test findAll method'
+  function cb (errorMessage, data) { }
+  const cbSpy = this.spy(cb)
+
+  const tableNameStub = this.stub(
+    CONNECTOR,
+    'getTableName', 
+    (Model) => { 
+      return 'test'
+    }
+  )
+
+  const getPrimaryKeyColumnStub = this.stub(
+    CONNECTOR, 
+    'getPrimaryKeyColumn', 
+    (Model) => { 
+      	return true
+    }
+  )
+
+  const escapeKeysStub = this.stub(
+		CONNECTOR,
+		'escapeKeys',
+		(Model) => {
+			return ["'title'", "'content'"]
+		}
+	)
+
+  const getInstanceFromRowStub = this.stub(
+		CONNECTOR,
+		'getInstanceFromRow',
+		(Model, rows) => {
+			return 'data'
+		}
+	)
+
+  const ArrowCollectionMock = (function () {
+    function ArrowCollectionMock() { }
+    return ArrowCollectionMock
+  })()
+ 
+  const arrowCollectionSpy = this.spy(function () {
+    return sinon.createStubInstance(ArrowCollectionMock)
+  })
+ 
+  this.stub(arrow, 'Collection', arrowCollectionSpy)
+
+  const queryStub = this.stub(
+    CONNECTOR,
+    '_query',
+    (query, callback, executor) => {
+      executor([1,2,3])
+    }
+  )
+
+  findAllMethod.bind(CONNECTOR, Model, cbSpy)()
+  
+  t.ok(queryStub.calledOnce)
+  t.ok(cbSpy.calledOnce)
+  t.end()
+}))
+
+test('### Test findAll Method - primaryKey column ###', sinon.test(function (t) {
+  const Model = ARROW.getModel('Posts')
+  const data = 'Test findAll method'
+  function cb (errorMessage, data) { }
+  const cbSpy = this.spy(cb)
+
+  const tableNameStub = this.stub(
+    CONNECTOR,
+    'getTableName', 
+    (Model) => { 
+      return 'test'
+    }
+  )
+
+  const getPrimaryKeyColumnStub = this.stub(
+    CONNECTOR, 
+    'getPrimaryKeyColumn', 
+    (Model) => { 
+      	return false
+    }
+  )
+
+  const escapeKeysStub = this.stub(
+		CONNECTOR,
+		'escapeKeys',
+		(Model) => {
+			return ["'title'", "'content'"]
+		}
+	)
+
+  const getInstanceFromRowStub = this.stub(
+		CONNECTOR,
+		'getInstanceFromRow',
+		(Model, rows) => {
+			return 'data'
+		}
+	)
+
+  const ArrowCollectionMock = (function () {
+    function ArrowCollectionMock() { }
+    return ArrowCollectionMock
+  })()
+ 
+  const arrowCollectionSpy = this.spy(function () {
+    return sinon.createStubInstance(ArrowCollectionMock)
+  })
+ 
+  this.stub(arrow, 'Collection', arrowCollectionSpy)
+
+  const queryStub = this.stub(
+    CONNECTOR,
+    '_query',
+    (query, callback, executor) => {
+      executor([1,2,3])
+    }
+  )
+
+  findAllMethod.bind(CONNECTOR, Model, cbSpy)()
+  
+  t.ok(queryStub.calledOnce)
+  t.ok(cbSpy.calledOnce)
+  t.end()
+}))
+
+test('### Stop Arrow ###', function (t) {
+
+  ARROW.stop(function () {
+    t.pass('Arrow has been stopped!')
+    t.end()
+  })
+})
